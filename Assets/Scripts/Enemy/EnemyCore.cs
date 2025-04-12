81a3102dda804e57078601c6d1cc2131354b8785
Yiasin01 <arafathossain822@gmail.com>
2025-04-12 22:05:17 +0600

    Fixed issues in 'EnemyCore.cs'

--- Assets/Scripts/Enemy/EnemyCore.cs
+++ Assets/Scripts/Enemy/EnemyCore.cs
@@ -5,9 +5,17 @@ namespace Enemy
     [RequireComponent(typeof(Rigidbody2D))]
     public class EnemyCore : MonoBehaviour
     {
-        public float moveSpeed = 2f;
-        public float detectionRange = 5f;
+        [SerializeField] 
+        private float moveSpeed = 2f; // private field
 
+        // Public property to access and modify moveSpeed
+        public float MoveSpeed
+        {
+            get => moveSpeed;
+            set => moveSpeed = value;
+        }
+
+        public float detectionRange = 5f;
         [HideInInspector] public Transform player;
         [HideInInspector] public Rigidbody2D rb;
 
