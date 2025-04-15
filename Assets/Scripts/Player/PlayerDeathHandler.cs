35e17aaa39d0cdfc53b3990d4dd3d0ec2a9fa6fb
Yiasin01 <arafathossain822@gmail.com>
2025-04-15 22:03:20 +0600

    Solved issues in PlayerDeathHandler.cs

--- Assets/Scripts/Player/PlayerDeathHandler.cs
+++ Assets/Scripts/Player/PlayerDeathHandler.cs
@@ -5,23 +5,20 @@ using UI;
 using Game;
 using Player.Weapons;
 
-
 namespace Player
 {
     [RequireComponent(typeof(Health))]
     public class PlayerDeathHandler : MonoBehaviour
     {
-
         private WeaponHandler weaponHandler;
-        private Weapon equippedWeaponBeforeDeath;
         private InputManager inputManager;
-        
+
         [SerializeField] private GameObject deathScreenUI;
         private Animator animator;
         private Health health;
         private GameOverUI gameOverUI;
         private Rigidbody2D rb;
-        
+
         [SerializeField] private float respawnDelay = 2f;
 
         private void Awake()
@@ -29,11 +26,9 @@ namespace Player
             animator = GetComponent<Animator>();
             health = GetComponent<Health>();
             rb = GetComponent<Rigidbody2D>();
-
             health.OnDeath += HandleDeath;
             gameOverUI = FindObjectOfType<GameOverUI>();
             weaponHandler = GetComponent<WeaponHandler>();
-
         }
 
         private void HandleDeath()
@@ -41,18 +36,13 @@ namespace Player
             animator.SetTrigger("isDead");
             if (deathScreenUI) deathScreenUI.SetActive(true);
             if (gameOverUI) gameOverUI.Show();
-
             if (inputManager != null)
             {
                 inputManager.DisablePlayerInput();
             }
             weaponHandler.enabled = false;
             StartCoroutine(Respawn());
-         
-
         }
-
-        
 
         private IEnumerator Respawn()
         {
@@ -65,27 +55,21 @@ namespace Player
             }
             else
             {
-                // Just respawn this player at checkpoint
                 transform.position = CheckpointManager.Instance.GetCheckpoint();
+
                 if (inputManager != null)
                 {
                     inputManager.EnablePlayerInput();
                 }
+
                 GetComponent<Health>().ResetHealth();
-                //ensures 'you died' text doesn't appear after respawn
+
                 if (deathScreenUI) deathScreenUI.SetActive(false);
-
             }
-            if (equippedWeaponBeforeDeath != null && weaponHandler != null)
-            {
-                weaponHandler.EquipWeapon(equippedWeaponBeforeDeath);
-            }
-
 
             GameManager.Instance.SetupLevel(); // Let GameManager spawn new player
         }
-        
-        
+
         private void OnDestroy()
         {
             if (health != null)
