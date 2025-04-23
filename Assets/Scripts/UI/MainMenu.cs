8c4670ae05fc86c96a1bb08935ccfc8db9ed9645
Yiasin01 <arafathossain822@gmail.com>
2025-04-23 22:18:13 +0600

    Made static method (with a wrapper so Unity UI buttons can call .)

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,20 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Game
 {
-public class MainMenu : MonoBehaviour
-{
-  public void PlayGame()
-  {
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-  }
+    public class MainMenu : MonoBehaviour
+    {
+        /// <summary>
+        /// Static method to start the game by loading the next scene.
+        /// Cannot be directly called from Unity UI buttons via Inspector.
+        /// </summary>
+        public static void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
 
-  public void QuitGame()
-  {
-    Application.Quit();
-  }
-}
+        /// <summary>
+        /// Wrapper to call the static PlayGame method from UI Button.
+        /// </summary>
+        public void PlayGameFromButton()
+        {
+            PlayGame();
+        }
+
+        /// <summary>
+        /// Quits the application. Works in builds only.
+        /// </summary>
+        public static void QuitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
 }
