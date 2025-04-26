34298f219b9b4fbbb324ee27216a3a6b841a350a
Tazrian Rafin <trrafin5656@gmail.com>
2025-04-27 02:21:29 +0600

    updated script

--- Assets/Scripts/Enemy/Bosses/NightBorne/BossWeaponHitbox.cs
+++ Assets/Scripts/Enemy/Bosses/NightBorne/BossWeaponHitbox.cs
@@ -24,7 +24,7 @@ namespace Enemy.Bosses.NightBorne
             }
         }
 
-        private void UnfreezeTime()
+        private static void UnfreezeTime()
         {
             Time.timeScale = 1f;
         }
