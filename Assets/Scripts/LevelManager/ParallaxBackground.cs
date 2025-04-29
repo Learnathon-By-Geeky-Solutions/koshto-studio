2f2a3c24f2b1c4d910c81bfac1135ed942160397
Sami <samikarim191139@gmail.com>
2025-04-30 04:17:56 +0600

    implimnted parallax background for boss level

--- Assets/Scripts/LevelManager/ParallaxBackground.cs
+++ Assets/Scripts/LevelManager/ParallaxBackground.cs
@@ -1,24 +1,81 @@
 ﻿using UnityEngine;
 
-namespace Level
+public class ParallaxBackground : MonoBehaviour
 {
-    public class ParallaxLayer : MonoBehaviour
+    [System.Serializable]
+    public class ParallaxLayer
     {
-        [SerializeField] private float parallaxMultiplier = 0.5f;
-        private Vector3 previousCameraPosition;
-        private Transform cameraTransform;
+        public Transform layerParent;
+        [Range(0f, 1f)]
+        public float parallaxEffect = 0.5f;
+        private float spriteWidth;
+        private Vector3 startPos;
+        private Transform cam;
 
-        private void Start()
+        public void Initialize(Transform cameraTransform)
         {
-            cameraTransform = Camera.main.transform;
-            previousCameraPosition = cameraTransform.position;
+            cam = cameraTransform;
+            startPos = layerParent.position;
+
+            // Calculate width from first child sprite
+            if (layerParent.childCount > 0)
+            {
+                SpriteRenderer sr = layerParent.GetChild(0).GetComponent<SpriteRenderer>();
+                if (sr != null) spriteWidth = sr.bounds.size.x;
+            }
         }
 
-        private void LateUpdate()
+        public void UpdateLayer()
         {
-            Vector3 delta = cameraTransform.position - previousCameraPosition;
-            transform.position += new Vector3(delta.x * parallaxMultiplier, delta.y * parallaxMultiplier, 0);
-            previousCameraPosition = cameraTransform.position;
+            if (cam == null) return;
+
+            float distance = cam.position.x * parallaxEffect;
+            float temp = cam.position.x * (1 - parallaxEffect);
+
+            // Move entire parent
+            layerParent.position = startPos + Vector3.right * distance;
+
+            // Infinite scroll logic
+            if (temp > startPos.x + spriteWidth)
+            {
+                startPos.x += spriteWidth;
+            }
+            else if (temp < startPos.x - spriteWidth)
+            {
+                startPos.x -= spriteWidth;
+            }
+        }
+    }
+
+    public ParallaxLayer[] layers = new ParallaxLayer[7];
+    private Transform camTransform;
+
+    private void Start()
+    {
+        camTransform = Camera.main.transform;
+
+        for (int i = 0; i < layers.Length; i++)
+        {
+            if (layers[i].layerParent != null)
+            {
+                layers[i].Initialize(camTransform);
+
+                // Set Z position (further back = higher Z)
+                Vector3 pos = layers[i].layerParent.position;
+                pos.z = 10 + i;
+                layers[i].layerParent.position = pos;
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        foreach (var layer in layers)
+        {
+            if (layer.layerParent != null)
+            {
+                layer.UpdateLayer();
+            }
         }
     }
 }
