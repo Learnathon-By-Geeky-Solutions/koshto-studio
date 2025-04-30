ff9ca28f18890d118925d4c993aefa2a956e542c
Yiasin01 <arafathossain822@gmail.com>
2025-05-01 01:29:17 +0600

    Refresh

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,7 +8,7 @@ namespace Game
    
         public static void PlayGame()
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
 
         public static void PlayGameFromButton()
